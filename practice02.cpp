866e9821ae7471e553d06264eb5abe68a26a87d6
Asadullah Ibn Ferdous <ak2069353070@gmail.com>
2026-01-24 00:08:29 +0600

    24/1/26

---
+++ practice02.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace  std;
+
+//doing the bst code by using char 
+struct Node{
+    char key;
+    Node *left;
+    Node *right;
+    Node *parent;
+    
+    Node(int val){
+        key=val;
+
+    }
+}
+
+
+int main(){
+    
+    return 0;
+}
+
+
+
+
+
+
