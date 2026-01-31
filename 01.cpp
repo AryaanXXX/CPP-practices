09a6ee31c506e90adc926a26ef20c7841f40c9c5
Asadullah Ibn Ferdous <ak2069353070@gmail.com>
2026-02-01 00:02:53 +0600

    1/2/26

---
+++ 01.cpp
@@ -0,0 +1,46 @@
+// #include<iostream>
+// using namespace  std;
+
+// int main(){
+//     int a,b;
+
+
+// #include<iostream>
+// using namespace  std;
+
+// int main(){
+//     string a= "2";
+//     string b= "1";
+//     cout<<stoi(a)+stoi(b)<<endl;  //by using stoi we convert string to integer.
+
+//     string x;
+//     int y;
+//     cin>>x>>y;  //here one is string and another one is int.
+//     cout<<x << y<<endl;   //both in combined.
+//     return 0;
+// }
+
+
+
+
+
+#include<iostream>
+using namespace  std;
+
+int main(){
+    int first_number ={3};
+    int scound_number ={7};
+    cout<<"First number " <<first_number<<endl;
+    cout<<"Scound number "<<scound_number<<endl;
+
+    int sum=first_number+scound_number;
+    cout<<"sum is: "<<sum<<endl;
+    return 0;
+    
+}
+
+
+
+
+
+
