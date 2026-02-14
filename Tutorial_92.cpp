002b71255754388bc8f88f79a802d410e31c2db0
Asadullah Ibn Ferdous <ak2069353070@gmail.com>
2026-02-15 00:32:50 +0600

    15/2/26

---
+++ Tutorial_92.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    try{
+    int num1,num2;
+    cout<<"Enter num1 : "<<endl;
+    cin>>num1;
+    cout<<"Enter num2: "<<endl;
+    cin>>num2;
+    if(num2==0){
+        throw -1;
+    }
+    double result=(double)num1/num2;
+    cout<<"The result is "<<result<<endl;
+    }catch(...){
+        cout<<"Divide by zero is not possible "<<endl;
+    }
+}
